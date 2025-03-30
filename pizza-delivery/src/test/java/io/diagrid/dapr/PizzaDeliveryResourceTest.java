95997df1db1aeb5c3900946e3549ae05e9466810
Matheus Cruz <matheuscruz.dev@gmail.com>
2025-03-30 01:06:50 -0300

    adjust flaky test

--- pizza-delivery/src/test/java/io/diagrid/dapr/PizzaDeliveryResourceTest.java
+++ pizza-delivery/src/test/java/io/diagrid/dapr/PizzaDeliveryResourceTest.java
@@ -8,7 +8,9 @@ import jakarta.inject.Inject;
 import org.apache.http.entity.ContentType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.shaded.org.awaitility.Awaitility;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -18,42 +20,39 @@ import static io.restassured.RestAssured.given;
 @QuarkusTest
 class PizzaDeliveryResourceTest {
 
-    @Inject
-    ObjectMapper mapper;
+   @Inject
+   ObjectMapper mapper;
 
-    @Inject
-    SubscriptionResource subscriptionResource;
+   @Inject
+   SubscriptionResource subscriptionResource;
 
-    @Test
-    void should_call_deliver_endpoint_correctly() throws JsonProcessingException, InterruptedException {
-        // given
-        List<PizzaDeliveryResource.OrderItem> orderItems =
-                List.of(new PizzaDeliveryResource.OrderItem(PizzaDeliveryResource.PizzaType.pepperoni, 100));
-        Date now = new Date();
+   @Test
+   void should_call_deliver_endpoint_correctly() throws JsonProcessingException, InterruptedException {
+      // given
+      List<PizzaDeliveryResource.OrderItem> orderItems = List.of(
+            new PizzaDeliveryResource.OrderItem(PizzaDeliveryResource.PizzaType.pepperoni, 100));
+      Date now = new Date();
 
-        PizzaDeliveryResource.Order order = new PizzaDeliveryResource.Order(
-                UUID.randomUUID().toString(),
-                orderItems,
-                now);
+      PizzaDeliveryResource.Order order = new PizzaDeliveryResource.Order(UUID.randomUUID().toString(), orderItems,
+            now);
 
-        String requestBody = this.mapper.writeValueAsString(order);
+      String requestBody = this.mapper.writeValueAsString(order);
 
-        // when
-        given().contentType(ContentType.APPLICATION_JSON.getMimeType())
-                .body(requestBody)
-                .when().put("/deliver")
-                .then()
-                .statusCode(200);
-
-        Thread.sleep(12000);
-
-        // then
-        List<CloudEvent<PizzaDeliveryResource.Event>> events = subscriptionResource.getAllEvents();
-        Assertions.assertEquals(4, events.size(), "Four published event are expected");
-        Assertions.assertEquals(PizzaDeliveryResource.EventType.ORDER_ON_ITS_WAY, events.get(0).getData().type(), "The content of the cloud event should be the order-out-on-its-way event");
-        Assertions.assertEquals(PizzaDeliveryResource.EventType.ORDER_ON_ITS_WAY, events.get(1).getData().type(), "The content of the cloud event should be the order-out-on-its-way event");
-        Assertions.assertEquals(PizzaDeliveryResource.EventType.ORDER_ON_ITS_WAY, events.get(2).getData().type(), "The content of the cloud event should be the order-out-on-its-way event");
-        Assertions.assertEquals(PizzaDeliveryResource.EventType.ORDER_COMPLETED, events.get(3).getData().type(), "The content of the cloud event should be the order-completed event");
-
-    }
+      // when
+      given().contentType(ContentType.APPLICATION_JSON.getMimeType()).body(requestBody).when().put("/deliver").then()
+            .statusCode(200);
+      
+      Awaitility.await().atMost(Duration.ofMinutes(1)).pollInterval(Duration.ofSeconds(5)).untilAsserted(() -> {
+         List<CloudEvent<PizzaDeliveryResource.Event>> events = subscriptionResource.getAllEvents();
+         Assertions.assertEquals(4, events.size(), "Four published event are expected");
+         Assertions.assertEquals(PizzaDeliveryResource.EventType.ORDER_ON_ITS_WAY, events.get(0).getData().type(),
+               "The content of the cloud event should be the order-out-on-its-way event");
+         Assertions.assertEquals(PizzaDeliveryResource.EventType.ORDER_ON_ITS_WAY, events.get(1).getData().type(),
+               "The content of the cloud event should be the order-out-on-its-way event");
+         Assertions.assertEquals(PizzaDeliveryResource.EventType.ORDER_ON_ITS_WAY, events.get(2).getData().type(),
+               "The content of the cloud event should be the order-out-on-its-way event");
+         Assertions.assertEquals(PizzaDeliveryResource.EventType.ORDER_COMPLETED, events.get(3).getData().type(),
+               "The content of the cloud event should be the order-completed event");
+      });
+   }
 }
